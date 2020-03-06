6cb2405d7cf8baf77435f3f0d7b48a0c5d1d7c40
dilanrinconhbtn <937@holbertonschool.com>
2020-03-06 17:47:56 -0500

    Google authentication and go to welcome

---
+++ lib/Widgets/GoogleButton.dart
@@ -0,0 +1,40 @@
+import 'package:filo/Modal/GoogleAuthentication.dart';
+import 'package:filo/Views/Home/Home.dart';
+import 'package:filo/Views/Welcome/Welcome.dart';
+import 'package:flutter/material.dart';
+
+
+Widget signInButton() {
+  return OutlineButton(
+    splashColor: Colors.grey,
+
+    onPressed: () {
+        signInWithGoogle().whenComplete(() {
+          runApp(MaterialApp(home: Welcome()));
+      });
+    },
+    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(40)),
+    highlightElevation: 0,
+    borderSide: BorderSide(color: Colors.grey),
+    child: Padding(
+      padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Image(image: AssetImage("pictures/images/google_logo.png"), height: 35.0),
+          Padding(
+            padding: const EdgeInsets.only(left: 10),
+            child: Text(
+              'Sign in with Google',
+              style: TextStyle(
+                fontSize: 20,
+                color: Colors.grey,
+              ),
+            ),
+          )
+        ],
+      ),
+    ),
+  );
+}
